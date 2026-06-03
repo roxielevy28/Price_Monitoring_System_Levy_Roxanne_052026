0fab1d35065bc120140bc31b44c9609ad502cca6
Roxanne Levy <roxielevy28@yahoo.com>
2026-06-03 01:48:05 -0400

    cimmit updates

--- scrapetest.py
+++ scrapetest.py
@@ -1,25 +1,20 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 page = requests.get('https://books.toscrape.com/catalogue/set-me-free_988/index.html')
 soup = BeautifulSoup (page.content, 'html.parser')
 print(soup)
-
 product = soup.find(id="default")
 print (product)
 
-items = product.find_all(class_= 'default')
+items = product.find_all(class_= 'container-fluid page')
 print(items)
 
+print(items[0].find(class_= 'product_pod').get_text())
 print(items[0].find(class_= 'star-rating Five').get_text())
 
+product_page_url = [items[0].find(class_= 'product_pod').get_text()]
 review_rating = [items[0].find(class_= 'star-rating Five').get_text()]
 
+print(product_page_url)
 print(review_rating)
-
-book_stuff = pd.DataFrame(
-    {
-     'review_rating': review_rating
-    })
-print(book_stuff)
