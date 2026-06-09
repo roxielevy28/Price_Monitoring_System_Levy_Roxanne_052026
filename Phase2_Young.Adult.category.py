8044d9e86d8fd13035d93be6011491366f97da01
roxielevy28 <roxielevy28@yahoo.com>
2026-06-09 23:25:51 +0000

    that's it for today

--- Phase2_Young.Adult.category.py
+++ Phase2_Young.Adult.category.py
@@ -1,3 +1,4 @@
+from urllib.parse import urljoin
 import pandas as pd 
 import request
 from bs4 import BeautifulSoup
@@ -5,3 +6,8 @@ from bs4 import BeautifulSoup
 catergory= requests.get('https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html')
 soup = BeautifulSoup (page.content, 'html.parser')
 
+books_on_page= product.find_all(class_= 'product_pod')
+
+raw_link=(books_on_page[0].find('h3').find('a')['href'])
+Actual_link= urljoin("https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html", raw_link)
+print(Actual_link)
